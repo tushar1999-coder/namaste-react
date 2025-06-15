38d1644d11aaef770f6897180a9bd6cc52bff4c7
Tushar <tusharvishi2006@gmail.com>
2025-06-15 21:17:58 +0530

    Add search functionality and Handle edge cases regarding 0 items found

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,10 +4,21 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
   //local state variable - Super powerful variable
-  const [originalListOfRestaurants, setOriginalListOfRestaurants] = useState(
+  const [listOfRestaurants, setListOfRestaurants] = useState([]); //original list of restaurants from API
+  const [filteredListOfRestaurants, setFilteredListOfRestaurants] = useState(
     []
   );
-  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  // Multiple app states are there
+  // 1. "Initial Loading State"
+  // 2. "Showing Filtered Results"
+  // 3. "Showing All Results"
+  const [appState, setAppState] = useState("Initial Loading State");
+
+  // Whenever state variables update, react triggers a reconciliation cycle(re-renders the component)
+  console.log("Body Rendered");
 
   useEffect(() => {
     fetchData();
@@ -25,24 +36,87 @@ const Body = () => {
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants;
 
-    setOriginalListOfRestaurants(jsonResList);
     setListOfRestaurants(jsonResList);
+    setFilteredListOfRestaurants(jsonResList);
+
+    setAppState("Showing All Results");
   };
 
   // Conditional Rendering
+  if (filteredListOfRestaurants.length === 0) {
+    if (appState === "Initial Loading State") {
+      return <Shimmer />;
+    } else if (appState === "Showing Filtered Results") {
+      return (
+        <div className='error-container'>
+          <h1>No Restaurants Found</h1>
+          <p>Please try again</p>
+        </div>
+      );
+    } else if (appState === "Showing All Results") {
+      return (
+        <div className='error-container'>
+          <h1>No Restaurants Found</h1>
+          <p>Please reload the webpage</p>
+        </div>
+      );
+    }
+  }
 
-  return listOfRestaurants.length === 0 ? (
-    <Shimmer />
-  ) : (
+  return (
     <div className='body'>
       <div className='filter-container'>
+        <div className='search'>
+          <input
+            type='text'
+            placeholder='Search'
+            className='search-box'
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              //filter the restaurant cards and update the UI
+              //searchText
+              console.log(searchText);
+
+              if (searchText === "") {
+                setFilteredListOfRestaurants(listOfRestaurants);
+                setAppState("Showing All Results");
+                return;
+              }
+
+              // Filter the original list of restaurants based on the search text
+              const filteredList = listOfRestaurants.filter((res) => {
+                const ans1 = res.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+
+                const ans2 = res.info.cuisines
+                  .join(", ")
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+
+                return ans1 || ans2;
+              });
+
+              setFilteredListOfRestaurants(filteredList);
+              setAppState("Showing Filtered Results");
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className='filter-btn'
           onClick={() => {
-            const filteredList = listOfRestaurants.filter(
+            const filteredList = filteredListOfRestaurants.filter(
               (res) => res.info.avgRating >= 4.5
             );
-            setListOfRestaurants(filteredList);
+            setFilteredListOfRestaurants(filteredList);
+            setAppState("Showing Filtered Results");
           }}
         >
           Top Rated Restaurants (Rating &gt;= 4.5)
@@ -50,14 +124,15 @@ const Body = () => {
         <button
           className='reset-btn'
           onClick={() => {
-            setListOfRestaurants(originalListOfRestaurants);
+            setFilteredListOfRestaurants(listOfRestaurants);
+            setAppState("Showing All Results");
           }}
         >
           All Restaurants
         </button>
       </div>
       <div className='res-container'>
-        {listOfRestaurants.map((restaurant) => (
+        {filteredListOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
