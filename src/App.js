96cd18d396dec32808e6cb1c3f4bde00e7a2006c
Tushar <tusharvishi2006@gmail.com>
2025-06-19 14:56:42 +0530

    Add children routes and links

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
 
 const AppLayout = () => {
   return (
     <div className='app'>
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
@@ -21,15 +21,18 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    children: [
+      { path: "/", element: <Body /> },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
     errorElement: <Error />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
   },
 ]);
 
